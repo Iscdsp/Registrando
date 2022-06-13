75a1d0691a7a61542cd261e54b8c6ae596f74407
Caio <caiopacnunes@gmail.com>
2022-06-13 13:09:07 -0300

    Página sendo refatorada em Angular+8, aplicando conceitos do Bootcamp Santander 2022 FullStack

---
+++ App-Reinaldo-Fotos/src/app/components/pages/contact-page/form-contact-page/form-contact-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-contact-page',
+  templateUrl: './form-contact-page.component.html',
+  styleUrls: ['./form-contact-page.component.css']
+})
+export class FormContactPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
